016509020f818de825fa6d07dd27c6e56051d1c4
final3blindside <72025445+final3blindside@users.noreply.github.com>
2023-01-06 01:40:41 +0000

    Fixed repo structure

---
+++ Week 3/while_loop_1.py
@@ -0,0 +1,8 @@
+x = 1
+def attempt(n):
+  while x <= n:
+    print("Attempt " + str(x))
+    x += 1
+  print("Done!")
+
+attempt(6)
--- Week3/while_loop_1.py
+++
@@ -1,8 +0,0 @@
-x = 1
-def attempt(n):
-  while x <= n:
-    print("Attempt " + str(x))
-    x += 1
-  print("Done!")
-
-attempt(6)
